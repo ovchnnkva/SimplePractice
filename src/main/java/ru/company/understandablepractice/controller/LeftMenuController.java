3e2d40b510ceeff4411b40df91f2c0e0aaab78d9
Denis <81586568+IL2DG@users.noreply.github.com>
2024-07-22 17:00:32 +0300

    PP-23 добавил гет мапинг для левого меню

--- src/main/java/ru/company/understandablepractice/controller/LeftMenuController.java
+++ src/main/java/ru/company/understandablepractice/controller/LeftMenuController.java
@@ -1,9 +1,11 @@
 package ru.company.understandablepractice.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +23,8 @@ public class LeftMenuController {
 
     private final LeftMenuService leftMenuService;
 
+    @Operation(summary = "Левое меню", description = "Позволяет получить левое меню текущего пользователя")
+    @GetMapping("/{userId}")
     public LeftMenuResponse getLeftMenu(@PathVariable @Parameter(name = "ID Пользователя") long userId){
         return leftMenuService.getLeftMenu(userId);
     }
