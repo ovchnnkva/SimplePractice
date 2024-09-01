6d3cd337553314b1107a7a72122a08a5523be5d1
ovchnnkva <ov4innnickova@yandex.ru>
2024-09-01 13:42:09 +0300

    PRI-64 добавила пагинацию в поиск клиентов

--- src/main/java/ru/company/understandablepractice/repository/PersonRepository.java
+++ src/main/java/ru/company/understandablepractice/repository/PersonRepository.java
@@ -17,4 +17,13 @@ public interface PersonRepository extends JpaRepository<Person, Long> {
             "FROM Person p " +
             "WHERE LOWER(p.fullName) LIKE LOWER(CONCAT('%', :name,'%'))")
     Optional<List<Person>> findPersonsByName (@Param("name") String name);
+
+    @Query(value =
+            "SELECT p " +
+                    "FROM Person p " +
+                    "WHERE LOWER(p.fullName) LIKE LOWER(CONCAT('%', :name,'%')) " +
+                    "ORDER BY p.id DESC " +
+                    "OFFSET :offset ROWS " +
+                    "FETCH NEXT :limit ROWS ONLY")
+    Optional<List<Person>> findPersonsByNamePagination (@Param("name") String name, @Param("offset") long offset, @Param("limit") long limit);
 }
